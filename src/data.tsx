fafe645fc549708782c8b6997d7e620c2496e790
Matthis Perrin <matthis.perrin@gmail.com>
2022-01-17 12:46:56 +0100

    Fetch and parse all civilization name and images

--- src/data.tsx
+++ src/data.tsx
@@ -1,23 +1,34 @@
-import {AoeModel} from './model';
+import {HTMLElement, parse} from 'node-html-parser';
+
+import {AoeModel, Civ} from './model';
 
 export async function fetchData(): Promise<AoeModel> {
-  return new Promise<AoeModel>(resolve => {
-    setTimeout(() => {
-      resolve({
-        civs: [
-          {
-            name: 'Japanese',
-            imgUrl:
-              'https://static.wikia.nocookie.net/ageofempires/images/9/9a/CivIcon-Japanese.png/revision/latest/scale-to-width-down/208?cb=20191107173240',
-          },
-          {
-            name: 'Britons',
-            imgUrl:
-              'https://static.wikia.nocookie.net/ageofempires/images/a/ae/CivIcon-Britons.png/revision/latest/scale-to-width-down/208?cb=20191107173130',
-          },
-        ],
-      });
-      // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-    }, 2000);
-  });
+  const html = await fetch(
+    'https://ageofempires.fandom.com/wiki/Civilizations_(Age_of_Empires_II)'
+  ).then(async res => res.text());
+
+  const doc = parse(html);
+  return {
+    civs: doc
+      .querySelectorAll('h3')
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, no-null/no-null
+      .filter(el => el.nextElementSibling !== null && el.nextElementSibling.rawTagName === 'div')
+      .map(el => parseCiv(el.childNodes[0]?.text ?? '', el.nextElementSibling))
+      .sort((civ1, civ2) => civ1.name.localeCompare(civ2.name)),
+  };
+}
+
+const dummy =
+  'https://static.wikia.nocookie.net/ageofempires/images/9/9a/CivIcon-Japanese.png/revision/latest/scale-to-width-down/208?cb=20191107173240';
+
+export function parseCiv(name: string, node: HTMLElement): Civ {
+  //   console.log(name, node.querySelector('img')?.attributes);
+  const imgUrl =
+    node.querySelector('img')?.attributes['data-src'] ??
+    node.querySelector('img')?.attributes['src'] ??
+    '';
+  return {
+    name,
+    imgUrl,
+  };
 }
