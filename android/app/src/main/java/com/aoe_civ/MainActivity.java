150005a47be242d248da9bcfb0b9d5125b001dd1
Matthis Perrin <matthis.perrin@gmail.com>
2022-01-17 09:31:59 +0100

    Init project

---
+++ android/app/src/main/java/com/aoe_civ/MainActivity.java
@@ -0,0 +1,15 @@
+package com.aoe_civ;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "aoe_civ";
+  }
+}
