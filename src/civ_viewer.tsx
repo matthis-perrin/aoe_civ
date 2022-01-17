7cbb123ce599dca36448fcaff2a6f86f81bdfaad
Matthis Perrin <matthis.perrin@gmail.com>
2022-01-17 13:19:34 +0100

    UI for parsed data

--- src/civ_viewer.tsx
+++ src/civ_viewer.tsx
@@ -12,17 +12,44 @@ interface CivViewerProps {
 
 export const CivViewer: React.FC<CivViewerProps> = props => {
   const {civ, onDismiss} = props;
-  const {name, imgUrl} = civ;
+  const {name, imgUrl, specialty, uniqUnits, uniqTechs, bonuses, teamBonus} = civ;
 
   const handleBackPress = onDismiss;
 
   return (
     <Wrapper>
       <TouchableOpacity onPress={handleBackPress}>
-        <BackButton>Back</BackButton>
+        <Header>
+          <Icon source={{uri: imgUrl}} />
+          <Name>{name}</Name>
+        </Header>
       </TouchableOpacity>
-      <Icon source={{uri: imgUrl}} />
-      <Name>{name}</Name>
+      <Specialty>
+        <Label>Specialty</Label>
+        <Value>{specialty}</Value>
+      </Specialty>
+      <UniqueUnits>
+        <Label>Unique Unit</Label>
+        {uniqUnits.map(unit => (
+          <Value key={unit}>{unit}</Value>
+        ))}
+      </UniqueUnits>
+      <UniqueTechs>
+        <Label>Unique Techs</Label>
+        {uniqTechs.map(tech => (
+          <Value key={tech}>{tech}</Value>
+        ))}
+      </UniqueTechs>
+      <Bonuses>
+        <Label>Bonuses</Label>
+        {bonuses.map(bonus => (
+          <Value key={bonus}>{bonus}</Value>
+        ))}
+      </Bonuses>
+      <TeamBonus>
+        <Label>Team Bonus</Label>
+        <Value>{teamBonus}</Value>
+      </TeamBonus>
     </Wrapper>
   );
 };
@@ -30,8 +57,13 @@ CivViewer.displayName = 'CivViewer';
 
 const Wrapper = styled(View)`
   width: 100%;
-  height: 48px;
   display: flex;
+`;
+const Header = styled(View)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 64px;
 `;
 
 const Icon = styled(Image)`
@@ -43,6 +75,31 @@ const Name = styled(LightText)`
   font-size: 24px;
 `;
 
-const BackButton = styled(LightText)`
-  font-size: 24px;
+const Label = styled(LightText)`
+  font-weight: 500;
+  background-color: #ffffff33;
+`;
+const Value = styled(LightText)`
+  background-color: #ffffff11;
+  margin-top: 2px;
+`;
+
+const Specialty = styled(View)`
+  height: 48px;
+`;
+
+const UniqueUnits = styled(View)`
+  height: 64px;
+`;
+
+const UniqueTechs = styled(View)`
+  height: 220px;
+`;
+
+const Bonuses = styled(View)`
+  height: 256px;
+`;
+
+const TeamBonus = styled(View)`
+  height: 48px;
 `;
